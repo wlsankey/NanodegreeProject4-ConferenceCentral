53d1660c9c013acc564f207d00fd55e22cb75b8f
William Sankey <william.sankey@gmail.com>
2015-11-05 07:41:27 -0500

    Conference Central application -- getConferenceSessions method not returning data.

---
+++ ConferenceCentral_Complete/scratch test.py
@@ -0,0 +1,12 @@
+Conference(key=Key('Profile', 'william.sankey@gmail.com', 'Conference', 1), city=u'Chicago', description=None, endDate=None, maxAttendees=0, month=0, name=u'Wharton Finance', organizerUserId=u'william.sankey@gmail.com', seatsAvailable=0, startDate=None, topics=[u'Default', u'Topic'])
+
+
+
+
+
+
+
+
+{'speaker': u'Johnie Baker', 'duration': None, 'typeOfsession': u'workshop', 'date': None, 'websafeConferenceKey': u'ah5kZXZ-Y29uZmVyZW5jZS1hcHBsaWNhdGlvbi05NjlyNQsSB1Byb2ZpbGUiGHdpbGxpYW0uc2Fua2V5QGdtYWlsLmNvbQwLEgpDb25mZXJlbmNlGAEM', 'key': Key('Profile', 'william.sankey@gmail.com', 'Conference', 1, 'Session', 3001), 'name': u'William Sankey', 'start_time': None, 'highlights': None}
+
+request = Session()
